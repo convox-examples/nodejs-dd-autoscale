addf7d6fd016a44e3841a16149363582c58e2a43
Nick <nthoni@hotmail.com>
2023-01-18 12:44:40 -0500

    updated files

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,15 @@
 FROM node:10.16.3-alpine
+ARG MY_TEST=default
+
+ENV EVAR=${MY_TEST}
 
 WORKDIR /usr/src/app
 
 COPY . /usr/src/app
+
+RUN echo ${EVAR} > mytxt
+
+RUN npm install hot-shots
 
 EXPOSE 3000
 
--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const http = require('http');
+const fs = require('fs');
 
 const port = process.env.PORT
 
+var StatsD = require('hot-shots');
+var dogstatsd = new StatsD();
+
 const server = http.createServer((req, res) => {
+  dogstatsd.increment('page.views')
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\nI\'m: ' + process.env.APP + '\nBuild:' + process.env.BUILD + '\nRelease: ' + process.env.RELEASE + '\nrunning on: ' + process.env.RACK);
+  var dt = "";
+  try {
+    const data = fs.readFileSync('mytxt', 'utf8');
+    console.error(data);
+    dt = data;
+  } catch (err) {
+    console.error(err + dt);
+  }
+  res.end('Hello Thryv!\nI\'m: ' + process.env.APP + '\nBuild:' + process.env.BUILD + '\nRelease: ' + process.env.RELEASE + '\nrunning on: ' + process.env.RACK + '\ndata: ' + dt);
   console.log('PING');
 });
 
